af274dc1b92543a68bbdc62378c575e2845c1c6c
Burak Yapici <byapici95@gmail.com>
2024-03-28 23:34:18 +0300

    DesktopNotification First Version

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__ =="__main__":
+        notification.notify(
+            title = "Burak Yapıcı Masaüstü Uygulaması  :)",
+            message = " Çalışırsan Öğrenirsin Öğrenirsen Yaparsın :) \nMutlaka başarırsın \nBaşarmak zorundasın :)",
+
+            timeout = 7
+)
+
+        time.sleep(37)
